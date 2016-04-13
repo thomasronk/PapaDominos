c46ae168c427c7684de438cfe12bae99d8e02250
thomasronk <thomasronk@gmail.com>
2016-04-12 21:37:20 -0500

    added header on top on successfull login

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,43 @@
 $(document).ready(function(){
+
+	var original_cookie= document.cookie;
+	console.log(original_cookie);
+	
+	cookies_array = original_cookie.split(";");
+	//console.log(cookies_array);
+
+	cook_array = cookies_array[0].split("=")
+	//console.log(cook_array[1]);
+
+	if(cook_array!=""){
+		$(".hello_user_header").text("Hello "+cook_array[1]);
+		$(".header_top").show();
+		//console.log(cook_array);
+	}
+	else{
+		$(".header_top").hide();
+	}
+
+	$("#signout").click(function(){
+
+		//console.log("signout now!");
+		deleteAllCookies();
+		window.location.replace("http://localhost:8888/PapaDominos/index.html");
+
+	});
+
+	function deleteAllCookies() {
+		console.log("deleting all cookies");
+    	var cookies = document.cookie.split(";");
+
+    	for (var i = 0; i < cookies.length; i++) {
+	    	var cookie = cookies[i];
+	    	var eqPos = cookie.indexOf("=");
+	    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+	    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    	}
+	}
+
 	
 	$("#pizza").click(function(){
 		
